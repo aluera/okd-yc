6751ca3228604c62651aad473788d5ab58d95ada
nnorienn <greentech676@gmail.com>
2022-03-29 14:32:27 +0400

    .

--- scripts/generate-ssh-key.sh
+++ scripts/generate-ssh-key.sh
@@ -4,5 +4,5 @@ if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else 
     echo "Generating new SSH keypair for FCOS nodes..."
-    ssh-keygen -t rsa -f ./secrets/id_rsa -q -N ""
+    ssh-keygen -f ./secrets/id_rsa -q -N ""
 fi
